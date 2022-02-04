8d10e169cbdef6c7093b663f0e2cf9017b63d0c7
Mahesh <gmahesh9391@gmail.com>
2022-02-04 10:39:42 +0530

    Initial Commit

---
+++ Feb-04-22/ClimbingTheLeaderboard.py
@@ -0,0 +1,21 @@
+# Problem Link      : https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem
+# Difficulty Level  : Medium
+
+def climbingLeaderboard(ranked,playerScores):
+    ranked = sorted(list(set(ranked)),reverse=True)
+    rankings = []
+    for score in playerScores:
+        while ranked and score >= ranked[-1]:
+            ranked.pop()
+        rankings.append(len(ranked)+1)
+
+    return rankings
+
+
+if __name__ == '__main__':
+    noOfPlayers = int(input())
+    ranked = list(map(int,input().split()))
+    noOfGames = int(input())
+    playerScores = list(map(int,input().split()))
+    rankings = climbingLeaderboard(ranked,playerScores)
+    print(rankings)
