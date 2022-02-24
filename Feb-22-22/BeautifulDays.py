4840328f5ca15afa2d3c706165aba9c80b84682e
Mahesh <gmahesh9391@gmail.com>
2022-02-24 16:50:10 +0530

    Implemented the solution using the simple brute force technique. since the test cases are small in number it passed without any timeouts.

---
+++ Feb-22-22/BeautifulDays.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulDays' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER i
+#  2. INTEGER j
+#  3. INTEGER k
+#
+
+def beautifulDays(i, j, k):
+    # Write your code here
+    beautifulDaysCount = 0
+    for num in range(i,j+1):
+        rev = int(str(num)[::-1])
+        if abs(rev-num) % k == 0:
+            beautifulDaysCount += 1
+    return beautifulDaysCount
+if __name__ == '__main__':
+
+
+    first_multiple_input = input().rstrip().split()
+
+    i = int(first_multiple_input[0])
+
+    j = int(first_multiple_input[1])
+
+    k = int(first_multiple_input[2])
+
+    result = beautifulDays(i, j, k)
+
+    print(str(result) + '\n')
+
+
