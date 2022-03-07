67058e70a02ed3cb395d4e0a7514f1b64fb9ab24
Mahesh <gmahesh9391@gmail.com>
2022-03-07 11:50:40 +0530

    Implemented the solution using simple list operations and simple implementation techniques.

---
+++ February_Circuits/EmptyArrays.py
@@ -0,0 +1,24 @@
+# Problem Link:
+
+def solve(first, second):
+    count = 0
+
+    while len(first) != 0:
+        if first[0] == second[0]:
+            first.pop(0)
+            second.pop(0)
+        else:
+            temp = first[0]
+            first.pop(0)
+            first = first + [temp]
+
+        count += 1
+
+    return count
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    A = list(map(int,input().split()))
+    B = list(map(int,input().split()))
+    result = solve(A,B)
+    print(result)
--- February_Circuits/tests.py
+++ February_Circuits/tests.py
@@ -1,11 +1,23 @@
 import unittest
 import XORSum
+import EmptyArrays
 
 class XORSumTest(unittest.TestCase):
     def test1(self):
         self.assertEqual(0,XORSum.solve(0))
     def test2(self):
         self.assertEqual(1, XORSum.solve(5))
+class EmptyArraysTest(unittest.TestCase):
+    def test1(self):
+        A = [1,3,2]
+        B = [2,3,1]
+        self.assertEqual(6, EmptyArrays.solve(A,B))
+
+    def test2(self):
+        A = [4,2,3,6,1,5]
+        B = [5,2,3,1,6,4]
+        self.assertEqual(14, EmptyArrays.solve(A,B))
+
 
 if __name__ == '__main__':
     unittest.main()
