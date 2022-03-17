1bcdd14f2aca9b29ea0d52965c903cebaa32e984
Mahesh <gmahesh9391@gmail.com>
2022-03-17 16:52:39 +0530

    Added the unittests to test the file BalancedReversals.py.

--- Codechef_Starters/tests.py
+++ Codechef_Starters/tests.py
@@ -1,5 +1,6 @@
 import unittest
 import ChefAndWaterBottles
+import BalancedReversals
 
 class ChefAndWaterBottlesTest(unittest.TestCase):
     def test1(self):
@@ -17,6 +18,20 @@ class ChefAndWaterBottlesTest(unittest.TestCase):
         result = 3
         self.assertEqual(result, ChefAndWaterBottles.solve(N,X,K))
 
+class BalancedReversalsTest(unittest.TestCase):
+    def test1(self):
+        length = 5
+        bin_str = '01100'
+        min_bin_str = '00011'
+
+        self.assertEqual(min_bin_str, BalancedReversals.solve(bin_str, length))
+
+    def test2(self):
+        length = 4
+        bin_str = '0000'
+        min_bin_str = '0000'
+
+        self.assertEqual(min_bin_str, BalancedReversals.solve(bin_str, length))
 
 
 if __name__ == '__main__':
