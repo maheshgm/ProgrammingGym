d2ce51e176da32ef9cc9090cfe0721d705c92e24
Mahesh <gmahesh9391@gmail.com>
2022-03-17 16:35:51 +0530

    Implemented the solution using the basic implementation techniques.

--- Codechef_Starters/ChefAndWaterBottles.py
+++ Codechef_Starters/ChefAndWaterBottles.py
@@ -1,3 +1,15 @@
 # Problem Link : https://www.codechef.com/START30C/problems/CHEFBOTTLE
 
+def solve(N,X,K):
+    max_bottles = N
+    bottles_filled = K // X
+    max_bottles = min(bottles_filled, max_bottles)
 
+    return max_bottles
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for _ in range(test_cases):
+        N,X,K = map(int,input().split())
+        max_bottles = solve(N,X,K)
+        print(max_bottles)
